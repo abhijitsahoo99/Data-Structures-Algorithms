44ddd8afe55d7e6e10d3dc5f2926a92fa921d524
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-02-19 11:23:25 +0530

    Update linearSearch.cpp
    Added Linear Search Code

--- Searching/LinearSearch/linearSearch.cpp
+++ Searching/LinearSearch/linearSearch.cpp
@@ -1,1 +1,25 @@
-Hello
+#include <iostream>
+using namespace std;
+int linear_search(int arr[], int size, int target){
+ for(int i=0 ; i<size ; i++){
+   if(arr[i] == target){
+     return i;
+   }
+  }
+   return -1;
+}
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int size = 10;
+    int target = 8;
+    int index = linear_search(arr,size,target);
+    if(index == -1){
+      cout<<"Element not found";
+    }
+    else{
+      cout<<"Element fount at index = "<<index;
+    }
+    return 0;
+  }
+
+
