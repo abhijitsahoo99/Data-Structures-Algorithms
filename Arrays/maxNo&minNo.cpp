db8b2ce869c5ae93c21d089a5393a9578af3df51
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-03-10 14:54:40 +0530

    Update and rename maxNo.cpp to maxNo&minNo.cpp

---
+++ Arrays/maxNo&minNo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{  
+  int arr[] = {2,3,7,10,12,14,17,10,20,24,30,12,88,36,16};
+  int size = 15;
+  int maxi = INT_MIN;
+  int mini = INT_MAX;
+  for(int i=0 ; i<size ; i++)
+  {
+    if(arr[i] > maxi)
+    {
+      maxi = arr[i];
+    }
+  }
+  for(int i=0 ; i<size ; i++)
+  {
+    if(arr[i] < mini)
+    {
+      mini = arr[i];
+    }
+  }
+  cout<<"Maximum number inside the array is : "<<maxi<<endl;
+  cout<<"Minimum number inside the array is : "<<mini;
+}
