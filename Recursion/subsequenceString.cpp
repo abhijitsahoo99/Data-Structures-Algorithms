c1ede2cf1a9e9b56308ae199d38dbb9a78fb1e64
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-03-20 23:07:39 +0530

    Create subsequenceString.cpp

---
+++ Recursion/subsequenceString.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void subSequence(string str,string output,int i){
+  if(i>=str.length()){
+    cout<<output<<endl;
+    return;
+  }
+  subSequence(str,output,i+1);
+  output = output+str[i];
+  subSequence(str,output,i+1);
+  
+}
+
+int main(){
+  string str = "abc";
+  int i = 0;
+  string output = " ";
+  subSequence(str,output,i);
+  return 0;
+}
