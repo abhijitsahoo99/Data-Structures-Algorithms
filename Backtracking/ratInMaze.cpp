1744488412b869f411b0f40817841317a0cfd5e1
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-03-28 15:13:06 +0530

    Update ratInMaze.cpp

--- Backtracking/ratInMaze.cpp
+++ Backtracking/ratInMaze.cpp
@@ -1,96 +1,92 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-bool isSafe(int i, int j, int row, int col, int arr[][4],
- vector<vector<bool> > &visited) {
-  if( ((i>=0 && i < row) && (j >= 0 && j< col)) &&
-      (arr[i][j] == 1 ) &&
-      (visited[i][j]==false)) {
+bool isSafe(int srcx, int srcy, int row, int col, int maze[][4],
+            vector<vector<bool>> &visited) {
+  if (((srcx >= 0 && srcx < row) && (srcy >= 0 && srcy < col))
+    && (maze[srcx][srcy] == 1 )
+    && (visited[srcx][srcy] == false)) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
 
-void solveMaze(int arr[][4], int row,int col, int i, int j,
-vector<vector<bool> > &visited, vector<string>& path, string output  ) {
-
-  //base case
-  if(i == row-1 && j == col-1) {
-    //answer found
+void solveMaze(int maze[][4], vector<vector<bool>> &visited, int srcx,int srcy, int row, int col, vector<string>& path,string output) {
+  // base case
+  if (srcx == row-1 && srcy == col-1) {
+    // answer found
     path.push_back(output);
     return;
-  } 
-
-  //Down -> i+1, j
-  if(isSafe(i+1, j, row, col, arr, visited)) {
-    visited[i+1][j] = true;
-    solveMaze(arr, row, col, i+1, j, visited, path, output + 'D');
-    //backtrack 
-    visited[i+1][j] = false;
   }
 
-  //Left -> i, j-1
-  if(isSafe(i, j-1, row, col, arr, visited)) {
-    visited[i][j-1] = true;
-    solveMaze(arr, row, col, i, j-1, visited, path, output + 'L');
-    //backtrack 
-    visited[i][j-1] = false;
+  // down ; i+1,j ;
+  if(isSafe (srcx+1, srcy, row, col, maze, visited)) {
+    visited[srcx+1][srcy] = true;
+    solveMaze(maze, visited, srcx+1, srcy, row, col, path, output+'D');
+    // Backtrack
+    visited[srcx+1][srcy] = false;
   }
 
-  //Right -> i, j+1
-  if(isSafe(i, j+1, row, col, arr, visited)) {
-    visited[i][j+1] = true;
-    solveMaze(arr, row, col, i, j+1, visited, path, output + 'R');
-    //backtrack 
-    visited[i][j+1] = false;
+  //right ; srcx,srcy+1 ;
+    if(isSafe (srcx, srcy+1, row, col, maze, visited)) {
+    visited[srcx][srcy+1] = true;
+    solveMaze(maze, visited, srcx, srcy+1, row, col, path, output+'R');
+    // Backtrack
+    visited[srcx][srcy+1] = false;
   }
 
-  //Up-> i-1, j
-if(isSafe(i-1, j, row, col, arr, visited)) {
-    visited[i-1][j] = true;
-    solveMaze(arr, row, col, i-1, j, visited, path, output + 'U');
-    //backtrack 
-    visited[i-1][j] = false;
+//left ; srcx,srcy-1 ;
+  if(isSafe (srcx, srcy-1, row, col, maze, visited)) {
+    visited[srcx][srcy-1] = true;
+    solveMaze(maze, visited, srcx, srcy-1, row, col, path, output+'L');
+    // Backtrack
+    visited[srcx][srcy-1] = false;
   }
 
-
+  //up ; srcx-1,srcy ;
+    if(isSafe (srcx-1, srcy, row, col, maze, visited)) {
+    visited[srcx-1][srcy] = true;
+    solveMaze(maze, visited, srcx-1, srcy, row, col, path, output+'U');
+    // Backtrack
+    visited[srcx - 1][srcy] = false;
+  }
 }
 
 int main() {
-  int maze[4][4] = { {1,0,0,0}, 
-                 {1,1,0, 1},
-                 {1,1,0,0},
-                 {0,1,1,1}};
 
-  if(maze[0][0] == 0) {
-    cout << "No Path Exists " << endl;
+  // Creating Maze Array
+  int maze[4][4] = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
+  if (maze[0][0] == 0) {
+    cout << "Path doesn't exists" << endl;
     return 0;
   }
 
+  // Source's Index
+  int srcx = 0;
+  int srcy = 0;
+
+  // Creating Visited Array
   int row = 4;
   int col = 4;
-
-  vector<vector<bool> > visited(row, vector<bool>(col,false));
-  //Marking Src Value true
+  vector<vector<bool>> visited(row, vector<bool>(col, false));
   visited[0][0] = true;
 
   vector<string> path;
   string output = "";
 
-  solveMaze(maze,row,col, 0,0, visited, path, output);
+  solveMaze(maze, visited, srcx, srcy, row, col, path, output);
 
-  cout << "printing the results " << endl;
-  for(auto i: path) {
-    cout << i << " ";
-  }
-  cout << endl;
-  if(path.size() == 0 ){
-    cout << "No Path Exists " << endl;
+  // Printing possible paths
+  cout<<"The possible paths are : ";
+  for (int i = 0; i < path.size(); i++) {
+    cout<< path[i] << " ";
   }
 
-  
-  return 0;
+  if (path.size() == 0) {
+    cout << "Path doesn't exists" << endl;
+  }
 }
+*/
+//DDRDRR DRDDRR
